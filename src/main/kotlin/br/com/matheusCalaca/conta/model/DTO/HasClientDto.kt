c8e239d677a1d90c653c557a3088c5436f591efa
matheusCalaca <matheusfcalaca@gmail.com>
2021-02-21 20:08:43 -0300

    adicionar secudary constructor

--- src/main/kotlin/br/com/matheusCalaca/conta/model/DTO/HasClientDto.kt
+++ src/main/kotlin/br/com/matheusCalaca/conta/model/DTO/HasClientDto.kt
@@ -3,9 +3,13 @@ package br.com.matheusCalaca.conta.model.DTO
 class HasClientDto {
     lateinit var identify: String
 
+
+
     constructor(identify: String) {
         this.identify = identify
     }
+
+    constructor()
 
     override fun toString(): String {
         return "{\"identify\"=\"'$identify'\"}"
